f3b133ba6fc9b292a7fe878dbe51fa895dbf5675
gaoliang <igaoliang@gmail.com>
2018-08-10 15:47:57 +0800

    提交拆分http
    将使用到的常量定义到单独的包中

--- tests/HttpRespReqCombineTest.go
+++ tests/HttpRespReqCombineTest.go
@@ -11,37 +11,35 @@ import (
 	_ "github.com/igaoliang/pcapbeat/structs"
 
 	"github.com/igaoliang/pcapbeat/structs"
-	"time"
+
 )
 
+
+
+
+type packetKey struct {
+	net, transport gopacket.Flow
+}
+
+// String prints out the key in a human-readable fashion.
+func (k packetKey) String() string {
+	return fmt.Sprintf("%v:%v", k.net, k.transport)
+}
+
+
+type combineHttpRecord struct{
+	pk packetKey
+	req string
+	resp string
+}
+
+// 用来保存request的数据流
+var reqMap = make(map[packetKey]combineHttpRecord)
+
+// 用来保存response的数据流
+var respMap = make(map[packetKey]combineHttpRecord)
+
 func main() {
-	/*path := "/Users/leo/Desktop/test.pcap"
-	handler, err := pcap.OpenOffline(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer handler.Close()
-
-	packetSource := gopacket.NewPacketSource(handler, handler.LinkType())
-
-	i := 0
-	for packet := range packetSource.Packets() {
-
-		i++
-
-		// fmt.Println(i)
-
-		x := printPacketInfo(packet)
-
-		fmt.Printf("xxxxxx is : %+v\n", x)
-
-		//printHttp(packet)
-	}
-
-	fmt.Println(i)*/
-
-
-
 
 	packetChan := make(chan structs.PcapStruct,10)
 
@@ -49,16 +47,14 @@ func main() {
 
 	for x := range packetChan{
 		fmt.Printf("*********************\n%+v\n",x)
-		time.Sleep(1000 * time.Duration(time.Millisecond))
+		//time.Sleep(1000 * time.Duration(time.Millisecond))
 	}
 
 }
 
 
 func AnalysisAndGenerate(packetChan chan structs.PcapStruct) {
-	//packetChan := make(chan structs.PcapStruct,10)
 
-	//path := "/Users/leo/Desktop/test.pcap"
 	path := "C:/Users/igaol/Desktop/tt.pcap"
 
 	handler, err := pcap.OpenOffline(path)
@@ -74,15 +70,14 @@ func AnalysisAndGenerate(packetChan chan structs.PcapStruct) {
 
 		i++
 
-		x := printPacketInfo(packet)
+		/*x := printPacketInfo(packet)
 
-		packetChan <- x
+		packetChan <- x*/
 
-/*		if i>100 {
-			break
-		}*/
 
-		fmt.Println(i)
+		combineHttp(packet)
+
+		//fmt.Println(i)
 
 	}
 
@@ -90,6 +85,172 @@ func AnalysisAndGenerate(packetChan chan structs.PcapStruct) {
 
 
 	//return packetChan
+}
+
+
+func combineHttp(packet gopacket.Packet) structs.PcapStruct{
+
+	//fmt.Println(">>>>>>>>>>>>>>>>>>>")
+
+	ps := structs.PcapStruct{
+		Ethernet: structs.Ethernet{
+			SrcMac:       "",
+			DstMAC:       "",
+			EthernetType: "",
+		},
+		Network: structs.Network{
+			SrcIP:    "",
+			DstIP:    "",
+			Protocol: "",
+		},
+		Transfer: structs.Transfer{
+			SrcPort: "",
+			DstPort: "",
+			Seq:     "",
+		},
+		Http: structs.Http{
+			Method:  "",
+			Payload: "",
+		},
+	}
+
+
+	// 数据链路层
+	// Let's see if the packet is an ethernet packet
+	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethernetLayer != nil {
+
+		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+
+		/*fmt.Println("Ethernet layer detected.")
+		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+		fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
+		fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
+		// Ethernet type is typically IPv4 but could be ARP or other
+		fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
+		fmt.Println()*/
+
+		ps.SrcMac = ethernetPacket.SrcMAC.String()
+		ps.DstMAC = ethernetPacket.DstMAC.String()
+		ps.EthernetType = ethernetPacket.EthernetType.String()
+
+	}
+
+	// 网络层
+	// Let's see if the packet is IP (even though the ether type told us)
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		//fmt.Println("IPv4 layer detected.")
+		ip, _ := ipLayer.(*layers.IPv4)
+
+		// IP layer variables:
+		// Version (Either 4 or 6)
+		// IHL (IP Header Length in 32-bit words)
+		// TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
+		// Checksum, SrcIP, DstIP
+		/*fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
+		fmt.Println("Protocol: ", ip.Protocol)
+		fmt.Println()*/
+
+		ps.SrcIP = ip.SrcIP.String()
+		ps.DstIP = ip.DstIP.String()
+		ps.Protocol = ip.Protocol.String()
+
+	}
+
+	// 传输层
+	// Let's see if the packet is TCP
+	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+	if tcpLayer != nil {
+		//fmt.Println("TCP layer detected.")
+		tcp, _ := tcpLayer.(*layers.TCP)
+
+		//tcp, some := tcpLayer.(*layers.TCP)
+		// tcp := tcpLayer
+		/*fmt.Println(reflect.TypeOf(tcp), reflect.TypeOf(tcpLayer))
+		// fmt.Println(tcpLayer)
+		fmt.Println("some=", some)
+
+		// TCP layer variables:
+		// SrcPort, DstPort, Seq, Ack, DataOffset, Window, Checksum, Urgent
+		// Bool flags: FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
+		// fmt.Printf("From port %d to %d\n", tcpLayer.SrcPort, tcpLayer.DstPort)
+		fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
+		fmt.Println("Sequence number: ", tcp.Seq)
+		fmt.Println()*/
+
+		ps.SrcPort = fmt.Sprintf("%d",tcp.SrcPort)
+		ps.DstPort = fmt.Sprintf("%d",tcp.DstPort)
+		ps.Seq = fmt.Sprint(tcp.Seq)
+
+	}
+
+
+	// When iterating through packet.Layers() above,
+	// if it lists Payload layer then that is the same as
+	// this applicationLayer. applicationLayer contains the payload
+	applicationLayer := packet.ApplicationLayer()
+	if applicationLayer != nil {
+
+		payloadString := string(applicationLayer.Payload())
+
+		if strings.Contains(payloadString, "HTTP") {
+
+			fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+			isReq := false
+			method := ""
+			// 遍历http的4种方法，一旦匹配到立马退出
+
+			for _, mthd := range utils.HttpMethodArray{
+				if strings.Contains(payloadString, mthd){
+					method = mthd
+					isReq = true
+					break
+				}
+			}
+
+			items := strings.Split(payloadString," ")
+
+			if isReq{
+				fmt.Println("--------this is a req streams.and method is", method)
+
+				fmt.Println(len(items))
+
+				fmt.Println(items[0],"--",items[1])
+
+
+			}else{
+				fmt.Println("-----------this is a resp streams.")
+				fmt.Println(items[0],"--",items[1])
+			}
+
+
+
+			fmt.Println("........",payloadString)
+
+			//fmt.Println("HTTP found!")
+
+			//fmt.Println("Application layer/Payload found.")
+
+
+
+			//fmt.Printf("%s\n", applicationLayer.Payload())
+
+			//fmt.Println("<<<<<<<<<<<<<<<<<<<")
+
+
+			ps.Payload = string(applicationLayer.Payload())
+
+		}
+	}
+
+	// Check for errors
+	if err := packet.ErrorLayer(); err != nil {
+		fmt.Println("Error decoding some part of the packet:", err)
+	}
+
+	return ps
 }
 
 
---
+++ utils/ConstUtil.go
@@ -0,0 +1,8 @@
+package utils
+
+
+const HttpMethodArray = [4]string{"GET","POST","UPDATE","DELETE"}
+
+var HttpCodeSummary = map[string]string{
+	"200":"ok",
+}
